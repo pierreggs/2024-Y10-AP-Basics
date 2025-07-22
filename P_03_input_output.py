c558cbe199a73ca051d571200d48615609246335
pierreggs <gonzalesp4476@masseyhigh.school.nz>
2025-07-22 12:56:07 +1200

    Getting input, attractive output

---
+++ P_03_input_output.py
@@ -0,0 +1,14 @@
+username = input("What is your name")
+
+fav_num = int(input("What is your favourite Number"))
+
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+print (f"\nHi {username}, your favourite number is {fav_num}")
+print(f"double {fav_num} is {double}")
+print(f"Half {fav_num} is {halve}")
+print(f"{fav_num} squared is {square}")
+print()
+print("Have a nice day.")
