64071e4c5f05ac265751c47778437ae57785c1c2
pierreggs <gonzalesp4476@masseyhigh.school.nz>
2025-07-30 13:06:52 +1200

    Area/Perimeter Calculator

---
+++ P_06_area_perim_calc.py
@@ -0,0 +1,9 @@
+width = float(input("Width: "))
+height = float(input("height: "))
+
+area = width * height
+perimeter = 2 * (width + height)
+
+print()
+print(f"Perimeter: {perimeter} units")
+print(f"Area: {area} square units")
